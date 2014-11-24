96e3ae43debb0fd95908e5df731f6434a0c5fc50
Skye Valde <skyevalde@gmail.com>
2014-11-24 10:53:38 -0600

    Changed how the products look. Updated styles

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 	def index 
-		@products = Product.order("name").page(params[:page]).per(2)
+		@products = Product.order("name").page(params[:page]).per(6)
 		@categories = Category.all
 	end
 
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
 
 	def products_in_category
 		@categories = Category.all
-		@products = Category.find(params[:id]).products.order("name").page(params[:page]).per(2)
+		@products = Category.find(params[:id]).products.order("name").page(params[:page]).per(6)
 		render:index
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	belongs_to :category
-	validates :name, :description, :price, :stock_quantity, presence: true
+	validates :name, :description, :price, :stock_quantity, :image, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :stock_quantity, numericality: { only_integer: true }
 end
